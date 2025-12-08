5deb05588c8337f154b8f3204326fffa3b3d3705
Charles Finkel <charles.finkel@gmail.com>
2025-12-08 17:54:33 -0500

    Initial Commit

---
+++ 868-hack/_68_hackApp.swift
@@ -0,0 +1,17 @@
+//
+//  _68_hackApp.swift
+//  868-hack
+//
+//  Created by Charles Finkel on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct _68_hackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
